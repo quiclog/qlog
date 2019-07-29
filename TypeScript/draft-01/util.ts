5a7b59bdba787f19c1a30ab0ab171c45597fe16c
Robin Marx <marx.robin@gmail.com>
2019-07-29 04:58:27 -0700

    Fix Util.ts to deal with missing EventTuple setup
    - Version published as 0.0.9 on NPM

--- TypeScript/draft-01/util.ts
+++ TypeScript/draft-01/util.ts
@@ -1,4 +1,4 @@
-import { PacketType, FrameTypeName, IEventTuple, EventCategory, EventType, EventTrigger, EventData } from './QLog'
+import * as qlog from './QLog';
 
 // usage:
 // either : 
@@ -9,9 +9,9 @@ import { PacketType, FrameTypeName, IEventTuple, EventCategory, EventType, Event
 // or :
 //      EventTupleWrapper.time(evt) 
 export class EventTupleWrapper{
-    public evt!:IEventTuple;
+    public evt!:Array<any>;
 
-    constructor(evt:IEventTuple | null){
+    constructor(evt:Array<any> | null){
         // if null you need to make sure to assign it yourself before using stuff
         // makes usage easier without having to create multiple instances
         // e.g.,
@@ -31,31 +31,44 @@ export class EventTupleWrapper{
         return 0;
     }
 
-    public get category() : EventCategory {
+    public get category() : qlog.EventCategory {
         return this.evt[1];
     }
 
-    public get type() : EventType {
+    public get type() : qlog.EventType {
         return this.evt[2];
     }
 
-    public get trigger() : EventTrigger {
+    public get trigger() : qlog.EventTrigger {
         return this.evt[3];
     }
 
-    public get data() : EventData {
+    public get data() : qlog.EventData {
         return this.evt[4];
     }
 
-    public static time(evt:IEventTuple)     : number {          return evt[0] }
-    public static category(evt:IEventTuple) : EventCategory {   return evt[1] }
-    public static type(evt:IEventTuple)     : EventType {       return evt[2] }
-    public static trigger(evt:IEventTuple)  : EventTrigger {    return evt[3] }
-    public static data(evt:IEventTuple)     : EventData {       return evt[4] }
+    public static time(evt:Array<any>)     : number {               return evt[0] }
+    public static category(evt:Array<any>) : qlog.EventCategory {   return evt[1] }
+    public static type(evt:Array<any>)     : qlog.EventType {       return evt[2] }
+    public static trigger(evt:Array<any>)  : qlog.EventTrigger {    return evt[3] }
+    public static data(evt:Array<any>)     : qlog.EventData {       return evt[4] }
 }
 
 export class QUtil {
-    public static WrapEvent(evt:IEventTuple | null) : EventTupleWrapper {
+    public static WrapEvent(evt:Array<any> | null) : EventTupleWrapper {
         return new EventTupleWrapper(evt);
+    }
+
+    public static TraceContainsSingleConnection(trace:qlog.ITrace) : boolean {
+
+        // a trace can conceptually contain more than 1 connection
+        // if there is just one, we expect there to be a "group_id" in the common_fields
+        // if there are more, we expect "group_id" in the event_fields
+        // if there isn't a group_id anywhere, we currently also assume there is just 1 connection
+        if( trace.event_fields.indexOf("group_id") >= 0 ){
+            return false;
+        }
+        else
+            return true;
     }
 }
